137f34f85b1d52add4a1e6c312d3624ccae09e3c
IvanHCenalmor <ivanhcenalmor@gmail.com>
2023-07-12 11:23:48 +0100

    Solved issues

--- jureqex/__main__.py
+++ jureqex/__main__.py
@@ -10,13 +10,13 @@ def main():
     
     :return: None
     """
-    parser = argparse.ArgumentParser(prog ='gfg',
+    parser = argparse.ArgumentParser(prog ='jureqex',
                                      description="Extracts requirements from a Jupyter notebook \
                                                  file and takes the versions from the installed packages.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--path", help="Path to the folder where the Jupyter notebook is.")
-    parser.add_argument("--name", help="Name of the Jupyter notebook.")
-    parser.add_argument("--save", help="Path with the name where the requirements are going to be saved.")
+    parser.add_argument("--path", help="Path to the folder where the Jupyter notebook is", required=True)
+    parser.add_argument("--name", help="Name of the Jupyter notebook", required=True)
+    parser.add_argument("--save", help="Path with the name where the requirements are going to be saved", required=True)
     args = vars(parser.parse_args())
     
     path_requiremts =  os.path.join(args["save"])
@@ -25,7 +25,7 @@ def main():
     notebook_requiremts = extract_requirements(nb_path)
 
     req_directory, req_name = os.path.split(path_requiremts)
-    path_notebook_requirements = os.ptah.join(req_directory, 'notebook_'+req_name)
+    path_notebook_requirements = os.path.join(req_directory, 'notebook_'+req_name)
 
     file=open(path_notebook_requirements,'w')
     file.writelines(f'# Requirements for {args["name"]}\n')
